06f27c47c475262b5ae8b60d17c49109e46801b6
letizia obiglio <letizia.obiglio@gmail.com>
2019-08-07 11:59:24 +0200

    add restaurants and reviews controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :restaurants, only: [:index]
+  resources :restaurants, only: [:index, :new, :create, :show] do
+    resources :reviews, only: [:new, :create]
+  end
 end
