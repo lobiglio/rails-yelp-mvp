b40589705c999b0753aa4325666ff4bd96093eac
letizia obiglio <letizia.obiglio@gmail.com>
2019-08-07 11:07:58 +0200

    Phone_number change to string in restaurants

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_07_090019) do
+ActiveRecord::Schema.define(version: 2019_08_07_090440) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "address"
-    t.integer "phone_number"
+    t.string "phone_number"
     t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
